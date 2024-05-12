63a121b7a9486a5a089127d183296ea765798d4c
Jaheda20 <jahedasultana20@gmail.com>
2024-05-12 22:56:09 +0200

    search functionality is applied on all jobs page

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -6,46 +6,40 @@ import { Link } from "react-router-dom";
 const AllJobs = () => {
 
     const [jobs, setJobs] = useState([]);
+    const [search, setSearch] = useState('')
+    const [searchText, setSearchText] = useState('')
 
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_API_URL}/jobs`)
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/allJobs?search=${search}`)
             console.log(data)
             setJobs(data)
         }
         getData()
 
-    }, [])
+    }, [search])
+
+    const handleSearch = e =>{
+        e.preventDefault();
+        
+        setSearch(searchText)
+    }
 
     return (
         <div>
             <div className="container mx-auto text-center flex flex-col items-center">
                 <h2 className="text-4xl dark:text-blue-400 text-blue-900 mt-20 mb-14">All Available Jobs</h2>
                 <div className="overflow-x-auto mb-10 border rounded-xl p-4">
-                    <form className="flex items-center justify-center ">
+                    <form onSubmit={handleSearch} className="flex items-center justify-center ">
                         <label className="input input-bordered flex items-center gap-2 my-4">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-                            <input type="text" className="w-96" placeholder="Enter Job Title" />
+                            <input type="text" className="w-96" name="search" onChange={e=>setSearchText(e.target.value)} value={searchText} placeholder="Enter Job Title" />
 
                             <button className='px-1 md:px-4 py-2 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
                                 Search
                             </button>
                         </label>
-                        {/* <div className='flex items-end justify-end p-1 overflow-hidden border rounded-lg w-1/2 focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
-                            <input
-                                className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
-                                type='text'
-                                // onChange={e => setSearchText(e.target.value)}
-                                // value={searchText}
-                                name='search'
-                                placeholder='Enter Job Title'
-                                aria-label='Enter Job Title'
-                            />
-
-                            <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
-                                Search
-                            </button>
-                        </div> */}
+                        
                     </form>
                     <table className="table">
 
